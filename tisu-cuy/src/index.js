e95e9bf3adf3ac2c8c0f96bb560778a303b65451
Luriahc <faqih.chairul08@gmail.com>
2025-11-08 04:01:07 +0800

    bahaya ini

--- tisu-cuy/src/index.js
+++ tisu-cuy/src/index.js
@@ -3,15 +3,17 @@ import cors from "cors";
 import morgan from "morgan";
 import mysql from "mysql2";
 import masterRoutes from "./routes/masterRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cleaning_system"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
